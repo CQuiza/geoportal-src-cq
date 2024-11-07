2b0d0d98b8e06a220488ad41c0d4e09524eb4f80
CQuiza <141818749+CQuiza@users.noreply.github.com>
2024-11-07 02:09:14 +0100

    add control layers group

--- src/app/components/pages/map/map.component.ts
+++ src/app/components/pages/map/map.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MapService } from '../../../services/map.service';
-import { icon, marker, Map, tileLayer } from 'leaflet';
+import { icon, marker, Map, tileLayer, layerGroup, control } from 'leaflet';
 import { MatButton } from '@angular/material/button';
 import { MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle } from '@angular/material/card';
 import { NgIf } from '@angular/common';
@@ -13,24 +13,40 @@ import { NgIf } from '@angular/common';
   styleUrl: './map.component.css'
 })
 export class MapComponent implements OnInit{
-
+  
   public map: any;
 
   public userLocation?: any;
   public mapService = inject(MapService)
+  public streetEsri: any = tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'});
+
+  public Esri_WorldTopoMap = tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community'});
+
+  public Esri_WorldImagery = tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'});
+
+  public baseMaps: {} = {
+    "streetMapEsri": this.streetEsri,
+    "topoEsri": this.Esri_WorldTopoMap,
+    "satelitalEsri": this.Esri_WorldImagery
+  };
+
+  public selectedLayer: string = 'streetEsri';
   public defaultLocation:[number,number] = [4.713, -74.086];
   constructor() { }
 
   ngOnInit(): void {
     this.mapService.getUserLocation();
+    this.setMapLayers(this.baseMaps);
   }
 
-  ngAfterViewInit(): void {
+  setMapLayers(baseMaps: {}): void {
     this.map = new Map('map').setView(this.defaultLocation, 10);
-    tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-      attribution: 'Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012'
-    }).addTo(this.map);
-  };
+    this.streetEsri.addTo(this.map);
+    control.layers(baseMaps).addTo(this.map);
+  }
 
   goUserLocation(): void {
 
@@ -41,10 +57,18 @@ export class MapComponent implements OnInit{
     }, 2000);
     this.userLocation = this.mapService.userLocation;
     if(this.userLocation){
-      const popupContent = `${this.userLocation[0]}, ${this.userLocation[1]}, ${this.userLocation[2].toFixed(3)}`;
+      var lat = this.userLocation[0]
+      var long = this.userLocation[1]
+      var accuracy = this.userLocation[2]
+      const popupContent = `${lat}, ${long}, ${accuracy.toFixed(3)}`;
       marker(this.userLocation).addTo(this.map).bindPopup(popupContent).openPopup();
       this.map.flyTo(this.userLocation, 15);
     }
   }
+
+
+
+
+
 
 }
